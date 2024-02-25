a33bd08da59f53b73a2d4692da4816cfcb08ca41
arielnotoreo <arielcthwe@gmail.com>
2024-02-24 19:21:34 -0500

    might have finished the logic for killing the plants

---
+++ js/game/classes.js
@@ -0,0 +1,67 @@
+class Plant
+{
+    //sprite, integer, integer, integer, integer, integer
+    constructor(pattern, timesWatered, waterFrequency, waterMaxLimit, lightFrequency, turningFrequency)
+    {
+        this.pattern = pattern;
+        this.waterFrequency = waterFrequency;
+        this.timesWatered = timesWatered;
+        this.waterMaxLimit = waterMaxLimit;
+        this.lightFrequency = lightFrequency;
+        this.turningFrequency = turningFrequency;
+
+        let daysSinceWatered = 0;
+        let timesWatered = 0;
+        let alive = true;
+    }
+
+    WaterTracker()
+    {
+        //initializing a timer
+        let timer;
+
+        //sets a timer for 60 seconds
+        startTimer()
+        {
+            timer = setTimeout(function(){}, 60000)
+        }
+
+        //resets the timer
+        resetTimer()
+        {
+            clearTimeout(timer);
+            startTimer();
+        }
+
+        WaterButtonClicked()
+        {
+            //check if the water button was clicked
+            if (waterButtonClicked == true)
+            {
+                //adjust values and reset the timer
+                daysSinceWatered = 0;
+                timesWatered++;
+                resetTimer();
+            }
+            else if (waterButtonClicked == false)
+            {
+                //adjust values and reset timer
+                daysSinceWatered++;
+                resetTimer();
+            }
+        }
+
+        startTimer();
+
+    }
+
+    //all health operations here
+    IsAlive()
+    {
+        //win conditions
+        if (waterFrequency < daysSinceWatered || timesWatered > waterMaxLimit)
+        {
+            alive = false;
+        }
+    }
+}
--- js/game/functions.js
+++ js/game/functions.js
@@ -1,7 +1,6 @@
-function DayCycle()
+//keeps track of the day counting and screen
+function DayCycle(dayCounter)
 {
-    let dayCounter = 0
-
     //this part can be subject to move to the main js file depending on how shit goes
     if (dayCounter % 2 == 0)
     {
@@ -11,4 +10,12 @@ function DayCycle()
     {
         //display nighttime screen
     }
+
+    setTimeout(function(){}, 30000);
+
+    //possibly figure out if we need a reset if game over
+
+    dayCounter++
+
+    return dayCounter;
 }
