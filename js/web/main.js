2baed765654925a80fc85ef7120f94639a20fb37
Leah Torregiano <lct2986@rit.edu>
2024-02-25 01:48:28 -0500

    testing user token

--- js/web/main.js
+++ js/web/main.js
@@ -3,7 +3,7 @@
 // Define the base URL for the Plant API
 const PLANTAPI_URL = "https://trefle.io";
 
-const token = "token=mJ0zOEvk8QqpnK5eijOVBWJPi09uU6XZyz2acO3L7rs";
+const token = "token=eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjozNDY1LCJv…Q.02vtbClEDZ8MdHzm30LBwI38IzC9pUnAw60a7-R-so8";
 let pageIndex = 1;
 
 // Execute when the window has loaded
